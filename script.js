f4c701816ae95ddf2dba4eb83a8f573241e6d806
Tom Leonards <tom.leonards2003@gmail.com>
2023-08-26 01:05:39 +0200

    add take card and rewride card draw method

--- script.js
+++ script.js
@@ -8,54 +8,97 @@ const valueP = document.getElementById('valueP');
 const valueC = document.getElementById('valueC');
 let IntvalueP = 0;
 let IntvalueC = 0;
+let playerCards = []
+let comCards = []
 
-
-
-// Warte, bis das DOM geladen ist
 document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById("start");
-    startButton.addEventListener("click", getCards);
-    const reset = document.getElementById("reset");
-    reset.addEventListener("click", resetSlots);
-
+    startButton.addEventListener("click", startGame);
+    const takeButton = document.getElementById("take");
+    takeButton.addEventListener("click", getCard);
+    const resetButton = document.getElementById("reset");
+    resetButton.addEventListener("click", resetSlots);
 });
 
-function getCards() {
 
-    placeCardInSlot(pCard1, "P");
-    placeCardInSlot(pCard2, "P");
 
-    placeCardInSlot(cCard1, "C");
-    placeCardInSlot(cCard2, "C");
 
-    const aliveCardsCount = deck.filter(card => !card.dead).length;
-    console.log(`Anzahl der lebenden Karten: ${aliveCardsCount}`);
+
+
+function startGame() {
+    placeCardInSlot("P");
+    placeCardInSlot("P");
+    placeCardInSlot("C");
+    placeCardInSlot("C");
+}
+
+function getCard() {
+    placeCardInSlot("P");
 }
 
 function resetSlots() {
-    pCard1.src = "/pictures/Empty.png";
-    pCard2.src = "/pictures/Empty.png";
-    cCard1.src = "/pictures/Empty.png";
-    cCard2.src = "/pictures/Empty.png";
     IntvalueC = 0;
     IntvalueP = 0;
+    playerCards = [];
+    comCards = [];
+
+    //Remove Cards from DOM
+    const cardBorderPlayer = document.getElementById("card-borderPlayer");
+    const cardBorderCom = document.getElementById("card-borderCom");
+    if (cardBorderPlayer) { removeChildNodes(cardBorderPlayer); }
+    if (cardBorderCom) { removeChildNodes(cardBorderCom); }
+
+    for (const card of deck) {
+        card.dead = false;
+    }
+
     valueP.textContent = `Value: 0`;
     valueC.textContent = `Value: 0`;
 }
 
-function placeCardInSlot(slot, mode) {
+function removeChildNodes(parentNode) {
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+}
+
+
+function placeCardInSlot(mode) {
     const card = getRandomAliveCard();
     if (card) {
         if (mode == "P") {
+            playerCards.push(card);
+            card.dead = true;
             IntvalueP += card.value;
             valueP.textContent = `Value: ${IntvalueP}`;
+
+            const cardborderPlayer = document.getElementById("card-borderPlayer");
+            const cardBorderP = document.createElement("div");
+            cardBorderP.classList.add("card-border");
+            const cardImageP = document.createElement("img");
+            cardImageP.classList.add("card");
+            cardImageP.src = playerCards[playerCards.length - 1].src;
+            cardImageP.alt = "Added Card";
+            cardBorderP.appendChild(cardImageP);
+            cardborderPlayer.appendChild(cardBorderP);
         }
         if (mode == "C") {
+            comCards.push(card);
+            card.dead = true;
             IntvalueC += card.value;
             valueC.textContent = `Value: ${IntvalueC}`;
+
+            const cardborderCom = document.getElementById("card-borderCom");
+            const cardBorderC = document.createElement("div");
+            cardBorderC.classList.add("card-border");
+            const cardImageC = document.createElement("img");
+            cardImageC.classList.add("card");
+            cardImageC.src = comCards[comCards.length - 1].src;
+            cardImageC.alt = "Added Card";
+            cardBorderC.appendChild(cardImageC);
+            cardborderCom.appendChild(cardBorderC);
+
         }
-        slot.src = card.src;
-        card.dead = true;
     }
 }
 
